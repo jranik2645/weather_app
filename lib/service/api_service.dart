5d7e1165cf5673acfe50377aa1b4ca50b88cc0ed
jranik2645 <mdjranik99@gmail.com>
2024-03-05 21:33:40 +0600

    Api base weather app

---
+++ lib/service/api_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:weather_app/constants/constant.dart';
+import 'package:weather_app/model/WeatherModel.dart';
+
+
+class ApiService {
+  Future<WeatherModel> getWeatherData(String searchText) async {
+    String url = "$base_url&q=$searchText&days=7";
+
+    try {
+      Response response = await get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        Map<String, dynamic> json = jsonDecode(response.body);
+        WeatherModel weatherModel = WeatherModel.fromJson(json);
+        return weatherModel;
+      } else {
+        throw ("No data found");
+      }
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+}
